c0cbc3f726aa8df3444696f9967c5596b4550bec
user <username@email.com>
2023-12-27 17:01:14 +0545

    replaced context.watch with consumer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,26 +8,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.read<ImagePickerService>().selectImage();
-              },
-              child: const Text("Open image"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(context.watch<ImagePickerService>().selectedFile?.name ?? "Nothing selected"),
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text("Video Player"),
       ),
+      body: Consumer<ImagePickerService>(builder: (context, value, child) {
+        return Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<ImagePickerService>().selectImage();
+                },
+                child: const Text("Open image"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(value.selectedFile?.name ?? "Nothing selected"),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
